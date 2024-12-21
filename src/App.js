e3951536c97167b0c53d062f86ad5cc0e220d8eb
Deepak Singh <ds8853845715@gmail.com>
2024-12-22 00:12:10 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center text-red-500">
+      <h1>Namastey React...</h1>
+    </div>
+  );
+}
+
+export default App;
